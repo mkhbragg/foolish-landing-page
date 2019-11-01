eeb1d339ecf292478fba94087d2a1bbadb7cfd75
mkhbragg <mkhbragg@gmail.com>
2019-11-01 11:04:22 -0400

    Incorporating create-react-app as separate frontend directory

--- minimal.py
+++ minimal.py
@@ -8,6 +8,7 @@ from django.core.management import execute_from_command_line
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 import os
 import json
@@ -30,11 +31,45 @@ settings.configure(
     MEDIA_ROOT = PROJECT_PATH + '/media/',
     STATIC_ROOT = PROJECT_PATH + '/static/',
     STATIC_URL='/static/',
+    INSTALLED_APPS = [
+        'webpack_loader'
+    ],
+    WEBPACK_LOADER = {
+        'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'dist/',
+            'STATS_FILE': os.path.join(PROJECT_PATH, 'webpack-stats.json'),
+        }
+    }
 )
+
+# def compareBenchmarks(stock):
+#     if stock['return_vs_benchmark']
+#         return
+
 
 
 def index(request):
-    return render(request, 'index.html')
+    temp = json.loads(recommendations(request).content)['recs']
+    recs = temp
+    # sorted(temp, temp.key=['benchmark_return'])
+    # temp.sort()
+    # print(temp)
+    for item in temp :
+        # print(key , " :: " , temp[key])
+        print("NEW ITEM :: ", item['benchmark_return'])
+    
+    return render(request, 'index.html', {
+        'recommendations': recs,
+        'topRecommendations': recs
+    })
+
+# def home(request):
+#     response = requests.get('http://freegeoip.net/json/')
+#     geodata = response.json()
+#     return render(request, 'core/home.html', {
+#         'ip': geodata['ip'],
+#         'country': geodata['country_name']
+#     })
 
 
 def ecap_ajax(request):
@@ -56,11 +91,12 @@ def recommendations(request):
     with open(disclosure_api_file, "r") as f:
         disclosure_data = json.load(f)
     sleep(1.75)
+    # return JsonResponse(data=disclosure_data, safe=False)
     return JsonResponse(data=disclosure_data)
 
 
 urlpatterns = [
-    url(r'^$', index),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'add_to_campaign', ecap_ajax),
     url('disclosure', disclosure),
     url('recs', recommendations),
