52a390f59d819893c5af2a4d6dd674060c3d2df7
mkhbragg <mkhbragg@gmail.com>
2019-11-04 13:56:58 -0500

    Including full footer text.

--- frontend/src/containers/Footer/Footer.tsx
+++ frontend/src/containers/Footer/Footer.tsx
@@ -5,7 +5,8 @@ import './Footer.scss';
 
 class Footer extends Component<any, any> {
     state = {
-        disclosure: null
+        disclosure: null,
+        today: new Date().getFullYear()
     }
 
     componentDidMount() {
@@ -23,9 +24,16 @@ class Footer extends Component<any, any> {
 
     render() {
         return (
-            <div className="Footer">
+            <footer className="Footer">
+                <p><a href="https://www.fool.com/PopUps/PrivacyInfo.htm" target="_blank">Privacy/Legal Information</a>.</p>
+                <p>
+                    Past performance is not a predictor of future results.
+                    Individual investment results may vary.
+                    All investing involves risk of loss.
+                </p>
                 { this.state.disclosure ? <div dangerouslySetInnerHTML={{ __html: this.htmlDecode(this.state.disclosure) }}></div> : null }
-            </div>
+                <p>&copy;1995-{this.state.today} The Motley Fool. All rights reserved. | <a href="https://www.fool.com/help/FoolMarks.htm" rel="noopener noreferrer" target="_blank">Legal Information</a></p>
+            </footer>
         );
     }
 }
